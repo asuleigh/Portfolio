45fa6de7f8b3dd72d2911044cf8c2f5a09d5c9b1
Amanda Surber <surberaleigh@gmail.com>
2020-12-04 01:30:34 -0500

    finished with the top part of the homepage and titles. Working on formatting About section and adding styles for links next

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,70 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
-import headshot from "../../src/headshot.jpg"
+// import headshot from "../../src/headshot.jpg"
 import background2 from "../../src/background2.jpg"
 import pic from "../../src/pic.jpg"
+
 
 const Home = () => (
 <div>   
     <Container fluid style={{ margin: 0, padding: 0 }}>
         <div>
         {/* <Card> */}
-        <Row style={{ backgroundImage: `url(${background2})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", width: "100vw", height: "100vh", borderBottomStyle: "ridge", borderColor: "#602040", margin: 0, padding: 0, overflow: "hidden" }}>
+        <Row style={{ backgroundImage: `url(${background2})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", width: "100vw", height: "100vh", borderBottomStyle: "ridge", borderColor: "#1d4d9a", margin: 0, padding: 0, overflow: "hidden" }}>
         <Col sm={12} md={12} lg={6} style={{ justifyContent: "center", alignItems: "center",}}>
         <Image fluid className="responsive" src={pic} roundedCircle style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "auto", width: "auto", margin: "0 auto", marginTop: "80px" }}/>
         </Col>
         <Col sm={12} md={12} lg={6} style={{ justifyContent: "center", alignItems: "center",}}>
-        <p style={{ textAlign: "center", color: "#eeeeee", fontFamily: "Lucida Handwriting", textShadow: "2px 2px 5px #1d4d9a", fontSize: "48px" }}>Welcome to my creative world!</p>
+        <p style={{ paddingTop:"250px", textAlign: "center", color: "#eeeeee", fontFamily: "Lucida Handwriting", textShadow: "2px 2px 5px #1d4d9a", fontSize: "48px" }}>Welcome to my creative world!</p>
+        <Nav style={{ paddingTop:"50px", display: "block", justifyContent: "center", alignItems: "center", textAlign: "center"}}>
+        <Nav.Item>
+        <Button variant="primary" size="lg" href="#about">
+        About, Links, and Contact
+        </Button>
+        </Nav.Item>
+        <br />
+        <Nav.Item>
+        <Button variant="primary" size="lg" href="#highlight">
+        Highlighted Work
+        </Button>
+        </Nav.Item>
+        </Nav>
         </Col>
         </Row>
         {/* </Card> */}
         </div>
-        <Row style={{ backgroundColor: "#fff2e6", width: "100vw", height: "350px", margin: 0, padding: 0 }}>
+        <Row id="about" style={{ backgroundColor: "#ffffcc", width: "100vw", height: "80vh", margin: 0, padding: 0 }}>
         <Col>
-            <h2>About</h2>
+        <p style={{ paddingTop:"50px", textAlign: "center", color: "#1d4d9a", fontFamily: "Lucida Handwriting", textShadow: "2px 2px 5px #1d4d9a", fontSize: "48px" }}>
+            About
+            </p>
+            <p>Originally from Southwestern Virginia, I hold a bachelor's degree from East Tennessee State University in Music Education with a background in teaching English, test scoring, content support, and supervising.
+            I recently earned a certificate from the Coding: Full-Stack program at the University of North Carolina at Charlotte. I enjoy being creative and meticulous with my work. Coding has allowed me to learn new skills and put to use many of the skills and discipline that I gained from being an educator and studying education.
+            Studying to be a web developer has put me on a path to a new career, as well as given me new confidence and a sense of accomplishment. I look forward to creating and collaborating in this new field. To contact me, set up a meeting, or request work on a project, please check the links in the Resume and Contact section.
+            Check out highlights of my coding portfolio below! 
+            </p>
         </Col>
         <Col>
-            <h2>Resume and Contact</h2>
+            <p style={{ paddingTop:"50px", textAlign: "center", color: "#1d4d9a", fontFamily: "Lucida Handwriting", textShadow: "2px 2px 5px #1d4d9a", fontSize: "48px" }}>
+            Resume and Contact
+            </p>
             <li>Resume</li>
             <li>LinkedIn</li>
             <li>GitHub</li>
             <li>Email</li>
         </Col>
         </Row>
-        <Row style={{ backgroundColor: "#fff2e6", width: "100vw", height: "400px", margin: 0, padding: 0 }}>
-        <h1>
+        <Row id="highlight" style={{ backgroundColor: "#ffffcc", width: "100vw", height: "80vh", margin: 0, padding: 0 }}>
+        <p style={{ textAlign: "center", color: "#1d4d9a", fontFamily: "Lucida Handwriting", textShadow: "2px 2px 5px #1d4d9a", fontSize: "48px" }}>
             Highlighted Projects
-        </h1>
-        <Carousel style={{ backgroundColor: "#fff2e6", width: "100vw", height: "350px", margin: 0, padding: 0 }}>
+            </p>
+        <Carousel style={{ backgroundColor: "#ffffcc", width: "100vw", height: "70vh", margin: 0, padding: 0 }}>
         <Carousel.Item>
         <img
             className="d-block w-100"
